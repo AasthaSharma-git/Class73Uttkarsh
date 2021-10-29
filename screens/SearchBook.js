f92c528782a41176e478f1cac3bdc1218531f62b
AasthaSharma-git <67312804+AasthaSharma-git@users.noreply.github.com>
2021-10-29 09:08:55 +0530

    Add files via upload

---
+++ screens/SearchBook.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View,Text} from 'react-native';
+
+class SearchBook extends React.Component{
+  render(){
+    return(
+       <View style={{alignItems:'center'}}>
+       <Text>Search</Text>
+       </View>
+
+    );
+
+  }
+}
+
+export default SearchBook;
